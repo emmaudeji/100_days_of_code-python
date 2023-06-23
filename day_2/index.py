d134c25a84c47f191d1ab45f6f725a17a26cc1ee
Emma Udeji <gogreneinfo@gmail.com>
2023-06-23 23:28:13 +0100

    initial commit

---
+++ day_2/index.py
@@ -0,0 +1,18 @@
+# convert data types
+num = 8
+print("Obi is " + str(num) + " years old.")
+float(8)
+int(num)
+
+# exercise
+print('Type checking challenge')
+a = input("Provide two digit numbers \n")
+
+print("the sum of the digit numbers is " + str(int(a[0]) + int(a[1])))
+76
+
+print(round(6.777777, 2))
+
+s = 9
+t = True
+print(f"is it {t} that obi is {s} years old")
